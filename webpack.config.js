468a7ef0c1e9550146d4870bf5dbfd550809c463
Mahmoud Adly <mahmoud.adly@gmail.com>
2016-08-10 22:07:19 +0200

    Pass node env to webpack for production compresssion.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,14 @@ var config = {
         loader : 'babel'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
 };
 
 module.exports = config;
